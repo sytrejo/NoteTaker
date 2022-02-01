928c94118bd61ddd16ea4dcd9c657be3d037dcc7
Silvia Trejo <s.y.trejo26@gmail.com>
2022-02-01 15:51:07 -0500

    Updating files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import NotesAPI from "./NotesAPI.js"
-
-NotesAPI.saveNote({
-    id: 224496,
-    title: "The title has changed",
-    body: "I am a new note."
-});
-
-console.log(NotesAPI.getALLNotes());
